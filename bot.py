bfbb887658f851d0fecce6eee10f16c952b7da48
Yusif Yusifov <komport@gmail.com>
2018-11-23 13:55:08 +0400

    App url

--- bot.py
+++ bot.py
@@ -28,7 +28,7 @@ def getMessage():
 @server.route("/")
 def webhook():
     bot.remove_webhook()
-    bot.set_webhook(url='' + TOKEN)
+    bot.set_webhook(url='https://dry-sea-53630.herokuapp.com/' + TOKEN)
     return "!", 200
 
 
