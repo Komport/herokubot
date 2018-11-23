fc44e27a38c2b29f019a4316b135eb70541f6ca0
Yusif Yusifov <komport@gmail.com>
2018-11-23 23:43:20 +0400

    setch fnc

--- bot.py
+++ bot.py
@@ -5,6 +5,7 @@ from flask import Flask, request
 import telebot
 from telebot import types
 
+import sketch
 TOKEN = '632985476:AAFfx7TD5eeKRhgexKBgMERA6zgD7znLFD4'
 bot = telebot.TeleBot(TOKEN)
 server = Flask(__name__)
@@ -18,18 +19,23 @@ def start(message):
 def open_menu(message):
     markup = types.ReplyKeyboardMarkup(row_width=2)
     btn1 = types.KeyboardButton('/youtube')
-    btn2 = types.KeyboardButton('/b')
+    btn2 = types.KeyboardButton('/sketch')
     btn3 = types.KeyboardButton('/c')
     btn4 = types.KeyboardButton('/d')
     markup.add(btn1,btn2,btn3,btn4)
     bot.reply_to(message,"Choose One letter:",reply_markup=markup)
 
-@bot.message_handler(commands=['youtube','b','c','d'])
+@bot.message_handler(commands=['youtube','sketch','c','d'])
 def menu_handler(message):
     if message.text == '/youtube':
         bot.reply_to(message,'You have selected '+message.text)
-    elif message.text == '/b':
-        bot.send_message(message.from_user.id,'You have selected: /b')
+    elif message.text == '/sketch':
+        msg = bot.send_message(message.from_user.id,'Send me your photo')
+        bot.register_next_step_handler(msg,photo_sketch)
+
+def photo_sketch(message):
+    bot.send_mesasge(message.from_user.id,message)
+
 
 @bot.message_handler(func=lambda message: True, content_types=['text'])
 def echo_message(message):
---
+++ sketch.py
@@ -0,0 +1,13 @@
+import imageio,numpy as np,scipy.ndimage
+
+def dodge(front,back):
+    result=front*255/(255-back) 
+    result[np.logical_or(result > 255, back ==255)] =255
+    return result.astype('uint8')
+
+def sketch_image(imge):
+    start_img = imageio.imread(imge)
+    gray_inv_img = 255-np.dot(start_img[...,:3], [0.299, 0.587, 0.114])
+    blur_img = scipy.ndimage.filters.gaussian_filter(inverted_img,sigma=5)
+    final_img= dodge(blur_img,gray_img)
+    return final_img
