fc76c41fde8a01e5a5bcdd33f3c1d2ad8a9ea4ed
Yusif Yusifov <komport@gmail.com>
2018-11-24 13:44:22 +0400

    sketch ffffix

--- sketch.py
+++ sketch.py
@@ -6,7 +6,8 @@ def dodge(front,back):
     return result.astype('uint8')
 
 def sketch_image(imge):
-    start_img = imageio.imread(imge)
+    #start_img = imageio.imread(imge)
+    start_img = imge
     gray_inv_img = 255-np.dot(start_img[...,:3], [0.299, 0.587, 0.114])
     blur_img = scipy.ndimage.filters.gaussian_filter(inverted_img,sigma=5)
     final_img= dodge(blur_img,gray_img)
